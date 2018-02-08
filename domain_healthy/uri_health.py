d4237a96120104974e8eb0b9b676fcbe00448dae
xuteng zhang <xtpeeps@qq.com>
2018-02-08 21:29:04 +0800

    Fix some bug.

--- domain_healthy/uri_health.py
+++ domain_healthy/uri_health.py
@@ -21,7 +21,7 @@ import requests
 import re
 import pdb
 from urllib2 import Request, urlopen, URLError, HTTPError
-
+import socket
 # 解决https报错：<urlopen error [SSL: CERTIFICATE_VERIFY_FAILED] certificate verify failed (_ssl.c:661)>，全局证书关闭
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
@@ -60,6 +60,10 @@ def output(texts):
 # 处理http请求
 # 输入：url
 # 输出：html(str),str(page.getcode())(str)；当无法连接时返回的是"can't connect.","error"
+# page:
+#   str,页面
+# rescode：
+#   str,返回http，https状态判断“access”“urlerror”“httperror”
 # 全局变量：重定向relocation
 # ---------------------------------------------------
 def gethttp(reurl_http):
@@ -88,7 +92,18 @@ def gethttp(reurl_http):
             relocation=opener.open(reurl_http)
             print "redirect:"+relocation
             output("redirect:"+str(relocation))
-        return page,str(response.getcode())
+            rescode=str(response.getcode())
+        head=response.info()
+        #print head
+        try:
+            re_header =head['server']
+            print "Server: "+re_header
+            server="Server: "+re_header
+            output(server)
+        except:
+            print "[-]Header didn't get server."
+            output("[-]Header didn't get server.")
+        return page,"access" #（等待开发，page通过opener获取，还没有合适的测试站，遇到再说）
     #
     # response error exception reload.
     #
@@ -98,27 +113,14 @@ def gethttp(reurl_http):
         exr='Error reason: '+str(e.code)
         print exr+' '+e.reason
         output(exr)
-        return "can't connect.","error"
+        return "can't connect.","httperror"
     except URLError, e:
         #print 'We failed to reach a server.'
         exr='Failed reason: '+str(e.reason)
         print exr
         output(exr)
-        return "can't connect.","error"
-    # except Exception,e:
-    #     print "ResponseError:",
-    #     #当使用不安全的sslv3连接的时候，<urlopen error [SSL: SSLV3_ALERT_HANDSHAKE_FAILURE] sslv3 alert handshake failure (_ssl.c:661)>，则报使用了不受支持的协议。
-    #     if str(e)=="<urlopen error [SSL: SSLV3_ALERT_HANDSHAKE_FAILURE] sslv3 alert handshake failure (_ssl.c:661)>":
-    #         e="[SSL: SSLV3_ALERT_HANDSHAKE_FAILURE]Using SSLv3 not secure."
-    #     elif str(e)=="<urlopen error [Errno 10061] >" or str(e)=="<urlopen error [Errno 10060] >":
-    #         e="[Errno 10060]Connection refused."
-    #     elif str(e)=="<urlopen error [Errno 11001] getaddrinfo failed>":
-    #         e="[Errno 11001]Cannot visit, no ip can't get host."
-    #     elif str(e)=="<urlopen error timed out>":
-    #         e="Time out."
-    #     print e
-    #     output("[+]ResponseError:"+str(e))
-    #     return "can't connect.","error"  #防止nonetype回传
+        return "can't connect.","urlerror"
+
 
 
 
@@ -137,17 +139,6 @@ def gethttp(reurl_http):
 #
 def getUrl_multiTry(url):
     user_agent ="Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.122 Safari/537.36"
-    #"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0"
-    #
-    # header_http={"Host": reurl_http,
-    # "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0"
-    # ,"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-    # "Accept-Language": "zh-CN",
-    # "Accept-Encoding": "gzip, deflate",
-    # #"Referer": "http://target.com/something_referer",
-    # "Content-Type": "application/x-www-form-urlencoded",
-    # "Connection": "close",
-    # "Upgrade-Insecure-Requests": "1"}
     headers = { 'User-Agent' : user_agent }
     maxTryNum=6
     html=u"init"
@@ -284,7 +275,7 @@ if __name__ == '__main__':
         print "[!]读取under_detection_targets.txt失败，请确保当前目录存在under_detection_targets.txt且含有内容。"
     for n in url_target:
         output(str(n))
-        print "[+]Target:"+n
+        print "Target:"+n
         #
         # 连接并接收http请求
         #
@@ -296,6 +287,20 @@ if __name__ == '__main__':
         response_http,pagecode_http=gethttp("http://"+n)
         response_https,pagecode_https=gethttp("https://"+n)
         #
+        # 针对三种返回状态进行处理,得出服务器关闭，无法访问页面，web服务正常，
+        #
+        try:
+            ip = socket.gethostbyname(n)
+            print "[+]Getip: "+ip
+            output("[+]Getip: "+ip)
+        except:
+            print "[!]Python socket couldn't get IP."
+            getip_error="[!]Python socket couldn't get IP."
+            output(getip_error)
+            pass
+            #(后续要补上)
+
+        #
         # 获取ping的情况
         #
         #
@@ -303,46 +308,6 @@ if __name__ == '__main__':
         print "end"
 
 
-
-
-        #
-        # print type(response_https)
-        # print response_https
-        # if response_https=="error":
-        #     print "[!]https connection error. \n"
-        # # print response_https,connect_mark_https,pagecode_https,
-        #
-        # #connect_markde的值0-http,1-https
-        # #
-        # # 内容检测部分
-        # soup_http=BeautifulSoup(response_http,'lxml')
-        # soup_https=BeautifulSoup(response_https,'lxml')
-        # # 符合白名单的直接标记无uri
-        # state_http=writename_check(soup_http)
-        # state_https=writename_check(soup_https)
-        # if state_http==0 or state_https==0:
-        #     continue
-        # # title判定
-        # title_state_http=title_check(soup_http)
-        # title_state_https=title_check(soup_https)
-        # if title_state_http==1 or title_state_https==1:
-        #     if (title_state_http==1 and pagecode_http==200) or (title_state_https==1 and pagecode_https==200):
-        #         output("访问正常"+'\n')
-        #     elif (title_state_http==0 and pagecode_http==200) or (title_state_https==0 and pagecode_https==200):
-        #         try:
-        #             htag_state_http=htag_check(soup_http)
-        #             htag_state_https=htag_check(soup_https)
-        #             if htag_state_http==0 or htag_state_https==0:
-        #                 continue
-        #         except:
-        #             pass
-        # elif (re(pagecode_http,r"30[0-9]") or re(pagecode_https,r"30[0-9]")):
-        #     #获取跳转之后的url
-        #     res = requests.head(url)
-        #     location=res.headers['Location']
-        #     output("可访问跳转"+location+'\n')
-        # else:
-        #     print 'continue.\n'
 
         # debug：正常情况测试
         #---------start----------
